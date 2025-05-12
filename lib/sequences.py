90bdeca434d54a6b778f506608bea724de7c2546
Techbandit001 <davidkiptum80@gmail.com>
2025-05-12 21:21:00 +0300

    adds sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+      fib = []
+      if length == 0:
+        print(fib)
+        return
+      elif length == 1:
+        fib = [0]
+      else:
+        fib = [0, 1]
+      while len(fib) < length:
+        fib.append(fib[-1] + fib[-2])
+      print(fib)
+pass
